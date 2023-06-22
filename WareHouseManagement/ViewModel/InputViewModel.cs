ee5dfd123dc67de8a9c4a0233b914f12ff052554
don't have any <vlad.luckashonok@ya.ru>
2023-06-22 17:14:31 +0300

    Inmplement imput vm

---
+++ WareHouseManagement/ViewModel/InputViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Input;
+using WareHouseManagement.Models;
+
+// ReSharper disable RedundantArgumentDefaultValue
+
+namespace WarehouseManagement.ViewModel
+{
+    public class InputViewModel : BaseViewModel
+    {
+        private string objectDisplayName;
+        private DateTime dateInput;
+        private int count;
+        private float priceInput;
+        private float priceOutput;
+        private string status;
+
+        public InputViewModel()
+        {
+            AddCommand = new RelayCommand(_ =>
+            {
+                var input = new InputInfo
+                {
+                    IdObject = ObjectDisplayName,
+                    Count = Count,
+                    InputPrice = PriceInput,
+                    OutputPrice = PriceOutput,
+                    Status = Status
+                };
+
+                DataProvider.Instance.DB.InputInfoes.Add(input);
+                DataProvider.Instance.DB.SaveChanges();
+            });
+        }
+
+        public string ObjectDisplayName
+        {
+            get => objectDisplayName;
+            set
+            {
+                objectDisplayName = value;
+                OnPropertyChanged(nameof(ObjectDisplayName));
+            }
+        }
+
+        public DateTime DateInput
+        {
+            get => dateInput;
+            set
+            {
+                dateInput = value;
+                OnPropertyChanged(nameof(DateInput));
+            }
+        }
+
+        public int Count
+        {
+            get => count;
+            set
+            {
+                if (value == 0) return;
+                count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+        public float PriceInput
+        {
+            get => priceInput;
+            set
+            {
+                priceInput = value;
+                OnPropertyChanged(nameof(PriceInput));
+            }
+        }
+
+        public float PriceOutput
+        {
+            get => priceOutput;
+            set
+            {
+                priceOutput = value;
+                OnPropertyChanged(nameof(PriceOutput));
+            }
+        }
+
+        public string Status
+        {
+            get => status;
+            set
+            {
+                status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        public ICommand AddCommand { get; }
+    }
+}
