e1b187eb9a881f88b324d7d4c33a073548d241a0
don't have any <vlad.luckashonok@ya.ru>
2023-06-23 12:24:31 +0300

    implement delete

--- WareHouseManagement/ViewModel/InputViewModel.cs
+++ WareHouseManagement/ViewModel/InputViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 using WareHouseManagement.Models;
 using Object = WareHouseManagement.Models.Object;
@@ -26,29 +27,24 @@ namespace WarehouseManagement.ViewModel
             Objects = new ObservableCollection<Object>(DataProvider.Instance.DB.Objects);
             List = new ObservableCollection<InputInfo>(DataProvider.Instance.DB.InputInfoes);
             
-            AddCommand = new RelayCommand(_ =>
+            AddCommand = new RelayCommand(param => this.AddExecuted(param),param => 
             {
-                var input = new Input
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    DateInput = DateInput
-                };
-                DataProvider.Instance.DB.Inputs.Add(input);
+                if (String.IsNullOrEmpty(Status)) return false;
+                return true;
+            });
+            
+            EditCommand = new RelayCommand(param => this.EditExecuted(param), p =>
+            {
+                if (String.IsNullOrEmpty(Status)) return false;
+                var displayList = DataProvider.Instance.DB.InputInfoes.Where(x => x.Object == SelectedObject);
+                if (displayList != null || displayList.Count() != 0) return true;
+                return false;
+            });
 
-                var inputInfo = new InputInfo
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    IdObject = SelectedObject.Id,
-                    IdInput = input.Id,
-                    Count = Count,
-                    InputPrice = PriceInput,
-                    OutputPrice = PriceOutput,
-                    Status = Status
-                };
-
-                DataProvider.Instance.DB.InputInfoes.Add(inputInfo);
-                DataProvider.Instance.DB.SaveChanges();
-                List.Add(inputInfo);
+            DeleteCommand = new RelayCommand(param => this.DeleteExecuted(param), p =>
+            {
+                if (SelectItem == null) return false;
+                return true;
             });
         }
 
@@ -135,7 +131,65 @@ namespace WarehouseManagement.ViewModel
                 OnPropertyChanged(nameof(Status));
             }
         }
+        
+        public RelayCommand AddCommand { get; }
+        public RelayCommand EditCommand { get; }
+        public RelayCommand DeleteCommand { get; }
+        
+        #region RelayCommand
 
-        public ICommand AddCommand { get; }
+        //Add new Object
+        private void AddExecuted(object param)
+        {
+            var input = new Input
+            {
+                Id = Guid.NewGuid().ToString(),
+                DateInput = DateInput
+            };
+            DataProvider.Instance.DB.Inputs.Add(input);
+
+            var inputInfo = new InputInfo
+            {
+                Id = Guid.NewGuid().ToString(),
+                IdObject = SelectedObject.Id,
+                IdInput = input.Id,
+                Count = Count,
+                InputPrice = PriceInput,
+                OutputPrice = PriceOutput,
+                Status = Status
+            };
+
+            DataProvider.Instance.DB.InputInfoes.Add(inputInfo);
+            DataProvider.Instance.DB.SaveChanges();
+            List.Add(inputInfo);
+        }
+        private void EditExecuted(object param)
+        {
+            var supplier = DataProvider.Instance.DB.InputInfoes
+                .Where(x => x.Id == SelectItem.Id).SingleOrDefault();
+            if (supplier != null)
+            {
+                supplier.OutputPrice = PriceOutput;
+                supplier.Count = Count;
+                supplier.Status = Status;
+                supplier.InputPrice = PriceInput;
+            }
+
+            DataProvider.Instance.DB.SaveChanges();
+            SelectItem.InputPrice = PriceInput;
+            SelectItem.OutputPrice = PriceOutput;
+            SelectItem.Count = Count;
+            SelectItem.Status = Status;
+        }
+        private void DeleteExecuted(object param)
+        {
+            var inputInfoes = DataProvider.Instance.DB.InputInfoes.Where(x => x.Id == SelectItem.Id).SingleOrDefault();
+            DataProvider.Instance.DB.InputInfoes.Remove(inputInfoes);
+            DataProvider.Instance.DB.SaveChanges();
+
+            List.Remove(inputInfoes);
+        }
+
+        #endregion RelayCommand
     }
 }
